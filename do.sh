811606605c5530aaa30963d306e4ac8d55b88fc4
Dennis E <git@destenson.digital>
2020-08-18 22:36:23 -0500

    do.sh & simple_do.sh

--- do.sh
+++ do.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 cc=$(which clang-3.6||which gcc-4.9||which clang||which gcc)
 so=$(test -f /etc/asl.conf && printf dylib|| printf so)
-$cc "-Dinline=__attribute__((__always_inline__))" -O3 -march=native -std=c11 -Wextra -Wpedantic -Wall -dynamic -shared keccak-tiny.c -fPIC -o libkeccak-tiny.$so -D"memset_s(W,WL,V,OL)=memset(W,V,OL)"
-$cc -Os -march=native -std=c11 -Wextra -Wpedantic -Wall -dynamic -shared keccak-tiny.c -fPIC -o libkeccak-tiny-small.$so -D"memset_s(W,WL,V,OL)=memset(W,V,OL)"
+ccargs='-march=native -Wextra -Wpedantic -Wall -dynamic -shared -fPIC -funroll-loops -funroll-all-loops'
+$cc "-Dinline=__attribute__((__always_inline__))" -O3 $ccargs keccak-tiny.c -o libkeccak-tiny.$so
+$cc -Os $ccargs keccak-tiny.c -o libkeccak-tiny-small.$so
--- simple_do.sh
+++ simple_do.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env sh
+
+name='keccak-tiny'
+
 cc=$(which clang-3.6||which gcc-4.9||which clang||which gcc)
 case $(uname) in
 Linux)
-    so=$(printf so) 
-    ARFLAGS='rvs -o'
+    # ARFLAGS='rvs -o'
+    # CFLAGS='-O3'
+    CFLAGS='-Os -std=c11 -Wextra -Wpedantic -Wall -funroll-loops -fPIC -dynamic -shared'
+    so=$(printf so)
+    $cc $name.c $CFLAGS -o lib$name.$so
     ;;
 Darwin)
-    so=$(printf dylib) 
+    so=$(printf dylib)
     ARFLAGS=rcs
     ;;
 MINGW*)
-    so=$(printf lib) 
+    so=$(printf lib)
     ARFLAGS='rcs -o'
     ;;
 *)
-    so=$(printf so) 
+    so=$(printf so)
     ARFLAGS='rvs -o'
     ;;
 esac
 # TODO: Fix the compiler to work with any of the above compilers $cc
-gcc -c keccak-tiny.c
-ar ${ARFLAGS} libkeccak-tiny.$so keccak-tiny.o
+
+case $(uname) in
+Linux)
+    # nothing needs to be done
+    ;;
+*)
+    $cc -c $name.c -o lib$name.$so
+    ar $ARFLAGS libkeccak-tiny.$so keccak-tiny.o
+    ;;
+esac
